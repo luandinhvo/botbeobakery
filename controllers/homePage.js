5ff786464ad153689b62b723daa55070b86608c8
luandinhvo <cv.dinhluan@gmail.com>
2020-07-05 11:28:00 +0900

    Thêm navibar

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,8 +1,8 @@
 const Post = require('../database/models/Post')
  
 module.exports = async (req, res) => {
-    const posts = await Post.find({});    
-    posts.reverse();
+    //const posts = await Post.find({});    
+    //posts.reverse();
 
     res.render("index", {
         posts
